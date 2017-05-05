d79188a1b9d924c1b183df81feb3488994a13084
gophper <990317429@qq.com>
2017-05-05 23:46:22 +0800

    cmd timeout

--- cmd.go
+++ cmd.go
@@ -2,7 +2,10 @@ package gomini
 
 import (
 	"bytes"
+	"errors"
 	"os/exec"
+	"time"
+	//"log"
 )
 
 func Exec(bin string, args ...string) (string, error) {
@@ -14,4 +17,40 @@ func Exec(bin string, args ...string) (string, error) {
 		return "", err
 	}
 	return out.String(), nil
+}
+func ExecWithTimeout(bin string, timeout time.Duration, args ...string) (string, error) {
+	cmd := exec.Command(bin, args...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Start()
+	if err != nil {
+		return "", err
+	}
+	err, isTimeout := CmdRunWithTimeout(cmd, timeout)
+	if err != nil {
+		return "", err
+	}
+	if isTimeout {
+		return "", errors.New("cmd run timeout")
+	}
+	return out.String(), nil
+}
+func CmdRunWithTimeout(cmd *exec.Cmd, timeout time.Duration) (error, bool) {
+	done := make(chan error)
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+	var err error
+	select {
+	case <-time.After(timeout):
+		// timeout
+		if err = cmd.Process.Kill(); err != nil {
+			//log.Error("failed to kill: %s, error: %s", cmd.Path, err)
+		}
+		<-done
+		return err, true
+	case err = <-done:
+		return err, false
+	}
 }
