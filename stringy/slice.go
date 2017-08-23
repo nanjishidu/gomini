e90275271e5fe8357f8ab1336a91a39a0255a287
nanjishidu <yijialiu212@gmail.com>
2017-08-23 22:50:06 +0800

    add stringy slice

---
+++ stringy/slice.go
@@ -0,0 +1,44 @@
+package stringy
+
+//https://github.com/nsqio/nsq/blob/master/internal/stringy/slice.go
+func Add(s []string, a string) []string {
+	for _, existing := range s {
+		if a == existing {
+			return s
+		}
+	}
+	return append(s, a)
+
+}
+
+func Union(s []string, a []string) []string {
+	for _, entry := range a {
+		found := false
+		for _, existing := range s {
+			if entry == existing {
+				found = true
+				break
+			}
+		}
+		if !found {
+			s = append(s, entry)
+		}
+	}
+	return s
+}
+
+func Uniq(s []string) (r []string) {
+	for _, entry := range s {
+		found := false
+		for _, existing := range r {
+			if existing == entry {
+				found = true
+				break
+			}
+		}
+		if !found {
+			r = append(r, entry)
+		}
+	}
+	return
+}
